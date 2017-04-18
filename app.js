d7153dda5d0a1a61a1d8fa73b1197f1884b77133
Miriam Elizabeth Laguna Velazquez <Miriam Elizabeth Laguna Velazquez>
2017-04-18 09:14:57 -0500

    ya terminado todo

--- app.js
+++ app.js
@@ -4,16 +4,23 @@ var elimPan1 = document.getElementById("eliminarPanda1");
 var elimPan2 = document.getElementById("eliminarPanda2");
 var elimPan3 = document.getElementById("eliminarPanda3");
 var elimPan4 = document.getElementById("eliminarPanda4");
-var fotoPanda1 = document.getElementById("panda1")
-var fotoPanda2 = document.getElementById("panda2")
-var fotoPanda3 = document.getElementById("panda3")
-var fotoPanda4 = document.getElementById("panda4")
+var fotoPanda1 = document.getElementById("panda1");
+var fotoPanda2 = document.getElementById("panda2");
+var fotoPanda3 = document.getElementById("panda3");
+var fotoPanda4 = document.getElementById("panda4");
+var text1 = document.getElementById("texto1");
+var text2 = document.getElementById("texto2");
+var ocultarIzq = document.getElementById("izquierda");
+var ocultarDer = document.getElementById("derecha");
+
 
 elimPan1.addEventListener("click",eliminar1);
 elimPan2.addEventListener("click",eliminar2);
 elimPan3.addEventListener("click",eliminar3);
 elimPan4.addEventListener("click",eliminar4);
-reiniciar.addEventListener("click",reiniciarTodo)
+reiniciar.addEventListener("click",reiniciarTodo);
+ocultarIzq.addEventListener("click",esconderIzq,"ondblclick",mostrarIzq);
+ocultarDer.addEventListener("click",esconderDer,"ondblclick",mostrarDer);
 
 function reiniciarTodo (){
   for(var i=0;i<restaurar.length;i++){
@@ -33,4 +40,16 @@ function eliminar3(){
 };
 function eliminar4(){
   fotoPanda4.style.display = "none";
+};
+function esconderIzq(){
+  text1.style.visibility = "hidden";
+};
+function esconderDer(){
+  text2.style.visibility = "hidden";
+};
+function mostrarIzq(){
+  text1.style.display = "block";
+};
+function mostrarDer(){
+  text2.style.display = "block";
 };
